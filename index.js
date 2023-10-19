dfbdf5218fd037d1d50684283ede8c2dc9f0296b
sakib kahadi <sakibkahadi72844@gamil.com>
2023-10-19 23:02:19 +0600

    added cart delete

--- index.js
+++ index.js
@@ -56,6 +56,7 @@ async function run() {
             const result = await cursor.toArray();
             res.send(result)
         })
+
         app.post('/carts', async(req,res)=>{
             const cart = req.body;
             console.log(cart);
@@ -64,6 +65,14 @@ async function run() {
             res.send(result)
             
         })
+        app.delete('/carts/:id', async(req,res)=>{
+            const id = req.params.id;
+            const query = {_id : id}
+            const result= await cartCollection.deleteOne(query)
+            res.send(result)
+        })
+        
+      
 
 
         // Send a ping to confirm a successful connection
