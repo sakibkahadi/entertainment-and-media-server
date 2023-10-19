421033c1ac413c8869b895b1b79098f3d9a656fb
sakib kahadi <sakibkahadi72844@gamil.com>
2023-10-19 20:59:04 +0600

    create a get for each products based on unique id

--- index.js
+++ index.js
@@ -1,17 +1,14 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion , ObjectId} = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
-
 //middlewares 
 app.use(cors());
 app.use(express.json())
 
 // mediaMingle
 // mLIRJK88dIoLhvUh
-
-//mongo db
 
 const uri = "mongodb+srv://mediaMingle:mLIRJK88dIoLhvUh@cluster0.9causbv.mongodb.net/?retryWrites=true&w=majority";
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -26,6 +23,34 @@ async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
+        
+        
+        const productsCollection = client.db('productsDB').collection('products')
+      
+        app.get('/products', async(req,res)=>{
+            const cursor = productsCollection.find()
+            const result = await cursor.toArray();
+            res.send(result)
+        })
+
+        app.post('/products' , async(req,res)=>{
+            const product =req.body;
+            console.log(product)
+            const result= await productsCollection.insertOne(product)
+            console.log(result)
+            res.send(result)
+        })
+
+        app.get('/products/:id', async(req,res)=>{
+            const id = req.params.id;
+            const query = {_id: new ObjectId(id)}
+            const result = await productsCollection.findOne(query)
+            res.send(result)
+        })
+        
+        
+
+
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
